725be1863dc11dfa6adfd6e2237bb43aadad79ec
JaimeLara <jrnst09@gmail.com>
2023-03-29 16:11:18 -0500

    Posting loading startes + toas 0.2

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -51,6 +51,14 @@ const CreatePostWizard = () => {
         type='text'
         value={input}
         onChange={(e) => setInput(e.target.value)}
+        onKeyDown={(e) => {
+          if (e.key === 'Enter') {
+            e.preventDefault()
+            if (input !== '') {
+              mutate({ content: input })
+            }
+          }
+        }}
         disabled={isPosting}
       />
       {input !== '' && !isPosting && (
