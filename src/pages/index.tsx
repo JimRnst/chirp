83d0bfd998d54a7bca2f9fbaa5ff583ca9ec4e71
JaimeLara <jrnst09@gmail.com>
2023-03-29 11:14:58 -0500

    loading states

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,6 +8,7 @@ import type { RouterOutputs } from "~/utils/api";
 
 import dayjs from "dayjs";
 import relativeTime from "dayjs/plugin/relativeTime";
+import { LoadingPage } from "~/components/loading";
 
 dayjs.extend(relativeTime);
 
@@ -45,15 +46,33 @@ const PostView = (props: PostWithUser) => {
   )
 }
 
+const Feed = () => {
+  const { data, isLoading: postsLoading } = api.posts.getAll.useQuery()
+
+  if(postsLoading) return <LoadingPage />
+
+  if(!data) return <div>Something went wrong</div>
+
+  return (
+    <div className='flex flex-col'>
+      {[...data, ...data]?.map((fullPost) => (
+        <PostView {...fullPost} key={fullPost.post.id} />
+      ))}
+    </div>
+  )
+}
+
+
 const Home: NextPage = () => {
 
-  const user = useUser()
+  const {isLoaded: userLoaded, isSignedIn} = useUser()
 
-  const { data, isLoading } = api.posts.getAll.useQuery()
+  //start fetching asap
+  api.posts.getAll.useQuery()
 
-  if (!data || isLoading) return <div>Loading...</div>
+  //Return empty div if both isen't loaded
+  if(!userLoaded ) return <div />
 
-  if (!data) return <div>Something went wrong</div>
 
   return (
     <>
@@ -65,22 +84,18 @@ const Home: NextPage = () => {
       <main className="flex justify-center h-screen">
         <div className='w-full h-full md:max-w-2xl border-x border-slate-400'>
           <div className='text-white border-b border-slate-400 p-4'>
-            {!user.isSignedIn &&
+            {!isSignedIn &&
               <div className="flex justify-center">
                 <SignInButton />
               </div>
             }
-            {user.isSignedIn && <CreatePostWizard />}
+            {isSignedIn && <CreatePostWizard />}
           </div>
-          <div className='flex flex-col'>
-            {[...data, ...data]?.map((fullPost) => (
-              <PostView {...fullPost} key={fullPost.post.id} />
-            ))}
-          </div>
+          <Feed />
         </div>
       </main>
     </>
-  );
-};
+  )
+}
 
-export default Home;
+export default Home
