f62be695a5784de9b8c831f3a1299b523b6e9604
JaimeLara <jrnst09@gmail.com>
2023-03-29 16:25:41 -0500

    New Files for new routes

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -11,6 +11,7 @@ import relativeTime from "dayjs/plugin/relativeTime";
 import { LoadingPage, LoadingSpinner } from "~/components/loading";
 import { useState } from "react";
 import { toast } from "react-hot-toast";
+import Link from "next/link";
 // import { toast } from "react-hot-toast";
 
 dayjs.extend(relativeTime);
@@ -84,8 +85,8 @@ const PostView = (props: PostWithUser) => {
       <Image src={author.porfilePicture} className='w-16 h-16 rounded-full' alt={`@${author.username}'s porfile picture`} width={56} height={56} />
       <div className='flex flex-col'>
         <div className="flex text-slate-300 font-bold gap-1">
-          <span>{`@${author.username}`}</span>
-          <span className='font-thin'>{`· ${dayjs(post.createdAt).fromNow()}`}</span>
+          <Link href={`/@${author.username}`}> <span>{`@${author.username}`}</span> </Link>
+          <Link href={`/post/${post.id}`}><span className='font-thin'>{`· ${dayjs(post.createdAt).fromNow()}`}</span></Link>
         </div>
         <span className="text-xl">{post.content}</span>
       </div>
